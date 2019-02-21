c5a93811be34222c0fa56eaab0da083adedc67e7
Postnov Daniil <dan.postnov@gmail.com>
2019-02-21 14:25:58 +0300

    add show text in case not found list items

--- src/components/ListAuto.vue
+++ src/components/ListAuto.vue
@@ -1,12 +1,18 @@
 <template>
-    <ul class="list-auto">
-        <li class="list-auto__item" v-for="car in cars" :key="car.id">
-            <ItemAuto
-                :car="car"
-                :userCoord="userCoord"
-                @setDistance="setDistance"/>
-        </li>
-    </ul>
+    <div class="list-auto">
+        <ul v-if="cars.length" class="list-auto__list">
+            <li class="list-auto__item" v-for="car in cars" :key="car.id">
+                <ItemAuto
+                    :car="car"
+                    :userCoord="userCoord"
+                    @setDistance="setDistance"/>
+            </li>
+        </ul>
+
+        <p v-else class="list-auto__empty">
+            По вашему запросу автомобили не найдены. Измените или очистите запрос.
+        </p>
+    </div>
 </template>
 
 <script>
@@ -27,14 +33,22 @@ export default {
 </script>
 
 <style>
-.list-auto > * { min-width: 0; }
+.list-auto__list > * { min-width: 0; }
 
-.list-auto {
+.list-auto__list {
     display: grid;
     grid-template-columns: repeat(4, 1fr);
     padding-left: 0;
     grid-gap: 20px 1.5%;
     list-style: none;
+}
+
+.list-auto__empty {
+    margin-top: 100px;
+    text-align: center;
+    font-size: 20px;
+    font-weight: bold;
+    color: #888;
 }
 
 </style>
